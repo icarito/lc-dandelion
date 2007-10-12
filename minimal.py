2bd9192438f199959267b220fd193a4b087409cb
dethe.elza <dethe.elza@8d484338-453c-0410-9b6b-614adad1ef0a>
2007-10-12 05:58:35 +0000

    adding minimal pygame bootstrap for testing

---
+++ minimal.py
@@ -0,0 +1,33 @@
+'''
+Minimal Pygame Startup
+
+Useful for testing things like getting sprites to rotate without jumping
+around.
+
+run with "python -i minimal.py" to play with it at the interactive prompt
+'''
+
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((200,200))
+sprite_image = pygame.image.load('spacewars/klingon.png')
+original_image = sprite_image
+black = (0, 0, 0, 255)
+
+def draw():
+   screen.fill(black)
+   screen.blit(sprite_image, sprite_image.get_rect(center=(100,100)))
+   pygame.display.update()
+
+def rotate1(angle):
+   global sprite_image
+   sprite_image = pygame.transform.rotate(original_image, angle)
+   draw()
+
+def rotate2(angle):
+   # Note: rotozoom had jaggies around the box of the image
+   global sprite_image
+   sprite_image = pygame.transform.rotozoom(original_image, angle, 1.0)
+   draw()
+
+draw()
