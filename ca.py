1535a206d2d569ebf982c3725ed5c99ec53333e7
dethe.elza <dethe.elza@8d484338-453c-0410-9b6b-614adad1ef0a>
2008-02-22 22:23:53 +0000

    added more standard functions to cellular automata generator

--- ca.py
+++ ca.py
@@ -29,10 +29,49 @@ def four_neighbors(matrix, x, y):
     height = len(matrix)
     return matrix[x-1][y], matrix[(x+1) % width][y], matrix[x][y-1], matrix[x][(y+1) % height]
     
-def eight_neighbors(matrix, x, y):
+def five_neighbors(matrix, x, y):
+    return four_neighbors(matrix, x, y) + (matrix[x][y],)
+    
+def four_sum(matrix, x, y):
+    return sum(four_neighbors(matrix, x, y))
+    
+def four_avg(matrix, x, y):
+    return four_sum(matrix, x, y) / 4.0 
+    
+def five_sum(matrix, x, y):
+    return sum(five_neighbors(matrix, x, y))
+    
+def five_avg(matrix, x, y):
+    return five_sum(matrix, x, y) / 5.0
+    
+def corner_neighbors(matrix, x, y):
     width = len(matrix[0])
     height = len(matrix)
-    return matrix[x-1][y], matrix[(x+1) % width][y], matrix[x][y-1], matrix[x][(y+1) % height], matrix[x-1][y-1], matrix[x-1][(y+1) % height], matrix[(x+1) % width][y-1], matrix[(x+1) % width][(y+1) % height]
+    return matrix[x-1][y-1], matrix[x-1][(y+1) % height], matrix[(x+1) % width][y-1], matrix[(x+1) % width][(y+1) % height]
+    
+def corner_sum(matrix, x, y):
+    return sum(corner_neighbors(matrix, x, y))
+    
+def corner_avg(matrix, x, y):
+    return corner_sum(matrix, x, y) / 4.0
+    
+def eight_neighbors(matrix, x, y):
+    four_neighbors(matrix, x, y) + corner_neighbors(matrix, x, y)
+    
+def nine_neighbors(matrix, x, y):
+    return five_neighbors(matrix, x, y) + corner_neighbors(matrix, x, y)
+    
+def eight_sum(matrix, x, y):
+    return sum(eight_neighbors(matrix, x, y))
+    
+def eight_avg(matrix, x, y):
+    return eight_sum(matrix, x, y) / 8.0
+    
+def nine_sum(matrix, x, y):
+    return sum(nine_neighbors(matrix, x, y))
+
+def nine_avg(matrix, x, y):
+    return nine_sum(matrix, x, y) / 9.0
             
 def generation_by_value(function, matrix):
     return [[function(matrix[x][y]) for y in glen(matrix[x])] for x in glen(matrix)]
@@ -52,7 +91,13 @@ def random_binary_matrix():
     
 def random_real_matrix():
     return initialize(lambda x,y: random.random())
-        
+    
+def clamp(value):
+    if value < 0.0: return 0
+    if value > 1.0: return 1.0
+    return value
+    
+    
 EMPTY_MATRIX = initialize(lambda x,y: 0)
 UNIT_MATRIX = initialize(lambda x,y: 1)
 
@@ -68,10 +113,17 @@ def test():
     view(EMPTY_MATRIX)
     
 def wave(matrix, x, y):
-    return sum(eight_neighbors(matrix, x, y)) / 8.0
+    C = matrix[x][y]
+    wave = .9 # not really sure what this should be
+    return clamp(C + (four_avg(matrix, x, y) - C))
     
 def wave_adapter(value):
     return 0, 0, int(value * 255)
+    
+def heat(matrix, x, y):
+    C = matrix[x][y]
+    DIFFUSE = 8.0 / 9.0
+    return C + DIFFUSE * (eight_avg(matrix, x, y) - C)
 
 def life(matrix, x, y):
     count = sum(eight_neighbors(matrix, x, y))
@@ -101,12 +153,17 @@ def pygame_test(matrix_init, generation_function, view_adapter):
         pygame.display.update()
         return generation_by_point(generation_function, matrix)
     while(True):
+        pygame.event.get()
+        x,y = pygame.mouse.get_pos()
+        if 0 < x < DEFAULT_WIDTH and 0 < y < DEFAULT_HEIGHT:
+            print x,y
+            matrix[x][y] = 1.0
         matrix = draw(matrix, sbuffer)
     
     
 if __name__ == '__main__':
     #test()
-#    pygame_test(random_real_matrix, wave, wave_adapter)
-    pygame_test(random_binary_matrix, life, life_adapter)
+    pygame_test(lambda: EMPTY_MATRIX, wave, wave_adapter)
+#    pygame_test(random_binary_matrix, life, life_adapter)
     
     
