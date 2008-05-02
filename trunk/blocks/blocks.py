eaca0bc7ecc7b38c40770af09490181208490042
dethe.elza <dethe.elza@8d484338-453c-0410-9b6b-614adad1ef0a>
2008-05-02 14:33:28 +0000

    using python to calculate the SVG for border, after finding the calculations to convert a rounded rect to a path.

---
+++ trunk/blocks/blocks.py
@@ -0,0 +1,55 @@
+'''
+via: http://www.w3.org/TR/SVG11/shapes.html#RectElement
+
+Mathematically, a 'rect' element can be mapped to an equivalent 'path' element as follows: (Note: all coordinate and length values are first converted into user space coordinates according to Units.)
+
+perform an absolute moveto operation to location (x+rx,y), where x is the value of the 'rect' element's x attribute converted to user space, rx is the effective value of the rx attribute converted to user space and y is the value of the y attribute converted to user space
+perform an absolute horizontal lineto operation to location (x+width-rx,y), where width is the 'rect' element's width attribute converted to user space
+perform an absolute elliptical arc operation to coordinate (x+width,y+ry), where the effective values for the rx and ry attributes on the 'rect' element converted to user space are used as the rx and ry attributes on the elliptical arc command, respectively, the x-axis-rotation is set to zero, the large-arc-flag is set to zero, and the sweep-flag is set to one
+perform a absolute vertical lineto to location (x+width,y+height-ry), where height is the 'rect' element's height attribute converted to user space
+perform an absolute elliptical arc operation to coordinate (x+width-rx,y+height)
+perform an absolute horizontal lineto to location (x+rx,y+height)
+perform an absolute elliptical arc operation to coordinate (x,y+height-ry)
+perform an absolute absolute vertical lineto to location (x,y+ry)
+perform an absolute elliptical arc operation to coordinate (x+rx,y)
+'''
+
+def block(width=200, height=45, radius=5, tabwidth=15, tabheight=10, armwidth=10):
+    '''
+    A basic, single-step block with upper slot, lower tab and no contents
+    '''
+    def L(x,y):
+        # Lx y
+        return 'A%d %d' % (x,y)
+    def A(x,y):
+        # Arx ry x-axis-rotation large-arc-flag sweep-flag x y
+        return 'L%d %d 0 0 0 %d %d' % (radius, radius, x, y)
+    path = ['M%d 0' % radius,
+        L(armwidth - radius, 0),
+        A(armwidth, radius),
+        L(armwidth, tabheight - radius),
+        A(armwidth + radius, tabheight),
+        L(armwidth + tabwidth - radius, tabheight),
+        A(armwidth + tabwidth, tabheight - radius),
+        L(armwidth + tabwidth, radius),
+        A(armwidth + tabwidth + radius, 0),
+        L(width - radius, 0),
+        A(width, radius),
+        L(width, height - radius),
+        A(width - radius, height),
+        L(armwidth + tabwidth + radius, height),
+        A(armwidth + tabwidth, height + radius),
+        L(armwidth + tabwidth, height + tabheight - radius),
+        A(armwidth + tabwidth - radius, height + tabheight),
+        L(armwidth + radius, height + tabheight),
+        A(armwidth, height + tabheight - radius),
+        L(armwidth, height + radius),
+        A(armwidth - radius, height),
+        L(radius, height),
+        A(0, height - radius),
+        L(radius, 0),
+        'z']
+    return ' '.join(path)
+    
+if __name__ == '__main__':
+    print block()
