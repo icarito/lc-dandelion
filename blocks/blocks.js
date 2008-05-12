ba04d91b70ec72df2cc5e456f47139a503dc5c8f
dethe.elza <dethe.elza@8d484338-453c-0410-9b6b-614adad1ef0a>
2008-05-12 14:14:35 +0000

    adding jquery and image snapshots, dragging works, dropping still needs some work

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,3 +1,32 @@
+function drag_out(e, ui){
+    // ui.instance = droppable
+    // ui.options 
+    // ui.position
+    // ui.absolute_position
+    // ui.draggable
+    // ui.helper
+    
+    // use to disconnect blocks
+    console.log('out');
+}
+
+function drag_drop(e, ui){
+    console.log('drop');
+}
+
+function drag_over(e, ui){
+    console.log('over');
+}
+
+function drag_activate(e, ui){
+    console.log('activate');
+}
+
+function drag_helper(e, ui){
+    return $('.drop_pointer', this);
+}
+
+
 function add_extraneous_elements(){
     $('.container').prepend(
         '<div class="top_left"></div>' + 
@@ -8,8 +37,13 @@ function add_extraneous_elements(){
         '<div class="bottom"></div>' + 
         '<div class="bottom_right"></div>'
     );
-    $('.block').prepend('<div class="right"></div>');
-    $('.trigger').prepend('<div class="right"></div>');
+    $('.block, .trigger').prepend('<div class="right"></div>');
+    $('.block, .container').prepend('<div class="drop_pointer"></div>');
+    $('.block, .container, .trigger').append('<div class="drop_target"></div>');
+    $('.block, .container, .trigger').draggable();
+//    $('.block, .trigger, .container').draggable({helper: drag_helper});
+//    $('.drop_target').droppable({activate: drag_activate});
+    $('.drop_target').droppable({accept: '.block, .container', hoverClass: 'drop_ok', out: drag_out, drop: drag_drop, over: drag_over});
 }
 
 $(add_extraneous_elements);
