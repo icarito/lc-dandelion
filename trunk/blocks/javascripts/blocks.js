fc4f68e2692f7427ae840eb14ddebb3cd6789f32
dethe.elza <dethe.elza@8d484338-453c-0410-9b6b-614adad1ef0a>
2008-05-25 19:23:40 +0000

    I have object dropping working now, with drag-over working correctly.  Drops are not always in the correct places, and there are still cases where they are not registered, but overall it is *much* better.  i.e., something is happening now.

--- trunk/blocks/javascripts/blocks.js
+++ trunk/blocks/javascripts/blocks.js
@@ -4,7 +4,22 @@ $.fn.extend({
     // positioning helper, returns {left, top, right, bottom}
     box: function(){
         var pos = this.offset();
-        return {left: pos.left, top: pos.top, right: pos.left + this.width(), bottom: pos.top + this.height()};
+        return {left: pos.left, 
+                top: pos.top, 
+                right: pos.left + this.width(), 
+                bottom: pos.top + this.height(),
+                intersects:function(box2){
+                    if (this.left > box2.right) return false;
+                    if (this.right < box2.left) return false;
+                    if (this.top > box2.bottom) return false;
+                    if (this.bottom < box2.top) return false;
+                    return true;
+                },
+                toString: function(){
+                    return 'box{left: ' + this.left + ', top: ' + this.top + ', right: ' + this.right + ', bottom: ' + this.bottom + '}';
+                }
+            };
+                
     },
     // positioning helper, returns boolean
     contains: function(other){
@@ -16,31 +31,47 @@ $.fn.extend({
         if (b2.bottom > b1.bottom) return false;
         return true;
     },
-    // positioning helper, returns intersects
+    // positioning helper, returns boolean
     intersects: function(other){
-        var b1 = this.box();
-        var b2 = $(other).box();
-        if (b2.left > b1.right){ this.log('other is to the right of this'); return false; }
-        if (b2.top > b1.bottom){ this.log('other is below this'); return false; }
-        if (b2.right < b1.left){ this.log('other is to the left of this'); return false; }
-        if (b2.bottom < b1.top){ this.log('other is above this'); return false; }
-        this.log('other intersects this');
-        return true;
+        return this.box().intersects(other.box());
+    },
+    up: function(expr){
+        var elem = this;
+//        console.log('up from ' + this.info() + ' to ' + expr);
+        while(true){
+            elem = elem.parent();
+//            console.log('up testing ' + elem.info() + ' against ' + expr);
+            if (elem.is(expr)){
+                return elem;
+            }else if(elem.is('body')){
+                return $();
+            }
+        }
     },
     // debug helper
     log: function(str){
         console.log(str);
         return this;
     },
-    // debug helper
-    firstinfo: function(){
-        var self = this.get(0);
-        return self.nodeName + '#' + self.id + '.' + self.className.split(' ').join('.')
+    first: function(){
+        return $(this.get(0));
+    },
+    label: function(){
+        var e = $('label', this);
+        if (!e.length){
+            e = $('label', this.parent());
+        }
+        return $.trim(e.first().text());
     },
     // debug helper
     info: function(){
+        var self = this.get(0);
+        return self.nodeName + '#' + self.id + '.' + self.className.split(' ').join('.') + ' (' + this.label() + ')';
+    },
+    // debug helper
+    infoall: function(){
         var output = ['matched ' + this.length + ' elements: '];
-        this.each(function(){output.push('\t' + $(this).firstinfo())});
+        this.each(function(){output.push('\t' + $(this).info())});
         return output.join('\n');
     },
     // debug helper
@@ -80,13 +111,43 @@ $.fn.extend({
             var id = this.id + '_drag_wrapper';
             self.subsequent().wrapAll('<div class="drag_wrapper" id="' + id + '"></div>');
             if (self.is('.loop')){
-                $('#' + id).draggable({handle: $('.top', self)});
+                $('#' + id).draggable({handle: $('.top', self), stop: stop_dragging, refreshPositions: true});
             }else{
-                $('#' + id).draggable({handle: self, stop: function(e,ui){$.ui.ddmanager.prepareOffsets(ui.draggable, e);}});
+                $('#' + id).draggable({stop: stop_dragging, refreshPositions: true});
             }
         });
     }
 });
+
+
+//  Structure of a block
+//
+//  .drag_wrapper
+//      .block (drag handle, if not a loop)
+//          .drop_pointer (if not a trigger)
+//          label
+//          [style holders] (drag handle is in here if a loop)
+//          [contained block .drag_wrappers]
+//          .drop_target
+//      .drag_wrapper for next block(s)
+//
+
+function stop_dragging(e, ui){
+    console.log('stop dragging helper: ' + ui.helper.info());
+    var drop = $.ui.ddmanager.last_droppable;
+    if (drop){
+        console.log('stop dragging drop: ' + drop.up('.block').info());
+    }
+    if (drop && drop.intersects($('.drop_pointer', ui.helper))){
+        console.log('appending ' + $('.block', ui.helper).info() + ' to ' + drop.up('.block').info());
+        ui.helper.css({position: 'relative', left: '0px', top: '0px'});
+        drop.up('.drag_wrapper').append(ui.helper);
+    }else{
+        console.log('appending ' + $('.block', ui.helper).info() + ' to document body');
+        $.ui.ddmanager.prepareOffsets(ui.helper, e);
+        $(document.body).append(ui.helper);
+    }
+}
 
 function drag_out(e, ui){
     // ui.instance = droppable
@@ -104,11 +165,11 @@ function drag_out(e, ui){
 function drag_drop(e, ui){
     var drop_elem = ui.element.get(0);
     var drag_elem = ui.draggable.get(0);
-    console.log('drop: ' + drop_elem + ', ' + drag_elem);
+    console.log('dropped ' + ui.draggable.info() + ' on ' + ui.element.info());
     try{
         ui.element.append(ui.draggable);
     }catch(e){
-        console.log('DOM exception when adding ' + ui.element.firstinfo() + ' to ' + ui.draggable.firstinfo());
+        console.log('DOM exception when adding ' + ui.element.info() + ' to ' + ui.draggable.info());
     }
     try{
         ui.element.css({position: 'relative', top: '0', left: '0'});
@@ -119,7 +180,7 @@ function drag_drop(e, ui){
 }
 
 function drag_over(e, ui){
-    console.log('over');
+    console.log(ui.draggable.info() + ' is over ' + ui.element.info());
 }
 
 function drag_activate(e, ui){
@@ -131,13 +192,23 @@ function drag_helper(e, ui){
 }
 
 function drop_accept(draggable){
-    return this.intersects($('.drop_pointer', draggable));
+    if (!draggable) return false;
+    var val = this.intersects($('.drop_pointer', draggable));
+    if (val){
+//        console.log(this.info() + ' will accept a drop of ' + draggable.info() + '?');
+        this.css('background-color', 'red');
+        $.ui.ddmanager.last_droppable = this;
+    }else{
+        this.css('background-color', 'yellow');
+//        $.ui.ddmanager.last_droppable = null;
+    }
+    return val;
 }
 
 // Make this a utility method
 function show_structure(e, level){
     e = $(e);
-    if (e.is('.trigger,.block,.loop')){
+    if (e.is('.block')){
         var output = [];
         for (var i = 0; i < level; i++){
             output.push('    ');
@@ -181,7 +252,9 @@ function setup_drag_and_drop(){
 //    $('.block').droppable({out: drag_out, drop: drag_drop, over: drag_over, activate: drag_activate});
     // Note, since this keeps tripping me up:
     // there are no callbacks fired unless there is an accept defined.  None.  And, of course, the accept has to match the actual draggable(s)
-    $('.block').droppable({over: drag_over, drop: drag_drop, accept: '.drag_wrapper', hoverClass: 'drop_ok'});
+//    $('.block .drop_target').droppable({over: drag_over, drop: drag_drop, accept: '.drag_wrapper', hoverClass: 'drop_ok'});
+    $('.block .drop_target').droppable({over: drag_over, drop: drag_drop, accept: drop_accept, hoverClass: 'drop_ok'});
+    
 }
 
 $(function(){
